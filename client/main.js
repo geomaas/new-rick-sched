b78f9a18eaebd6f4b22f98427186d56d272011dc
geomaas <geoff.maas@gmail.com>
2017-02-21 23:24:22 -0500

    started adding router

--- client/main.js
+++ client/main.js
@@ -1,11 +1,15 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
+import uiRouter from 'angular-ui-router';
 import scheduleRequest from '../imports/components/scheduleRequest/scheduleRequest.js';
+// import login from '../imports/components/login/login.js';
 import '../imports/startup/accounts-config.js';
 import {Requests, MonOne, MonTwo, TueOne, TueTwo, WedOne, WedTwo, ThuOne, ThuTwo, FriOne, FriTwo, SatOne, SatTwo, SunOne, SunTwo} from '../imports/api/requests.js';
 
 angular.module('schedule-app', [
   angularMeteor,
+  uiRouter,
   scheduleRequest.name,
+  // login.name,
   'accounts.ui'
 ]);
---
+++ imports/components/login/login.js
@@ -0,0 +1,20 @@
+// import angular from 'angular';
+// import angularMeteor from 'angular-meteor';
+//
+// import template from './login.html';
+//
+// class LoginCtrl {
+//     constructor($scope) {
+//         $scope.viewModel(this);
+//
+//
+//     }
+// }
+//
+// export default angular.module('login', [
+//         angularMeteor
+//     ])
+//     .component('login', {
+//         templateUrl: 'imports/components/login/login.html',
+//         controller: ['$scope', LoginCtrl]
+//     });
