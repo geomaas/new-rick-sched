7c3caa1bae7013033a907c92df09f4264906d8cc
geomaas <geoff.maas@gmail.com>
2017-03-04 14:51:12 -0500

    working on reset password

--- imports/components/login/login.js
+++ imports/components/login/login.js
@@ -1,66 +1,74 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import uiRouter from 'angular-ui-router';
-import { Accounts } from 'meteor/accounts-base'
+import {
+    Accounts
+} from 'meteor/accounts-base'
 //
 import template from './login.html';
 
 class LoginCtrl {
     constructor($scope, $state) {
-      'ngInject';
+        'ngInject';
         $scope.viewModel(this);
+
         this.$state = $state;
+
         this.hide = true,
-        this.signup = {
-        },
-        this.helpers({
-          currentUser(){
-            return Meteor.user();
-          }
-        })
+
+            this.signup = {},
+            this.helpers({
+                currentUser() {
+                    return Meteor.user();
+                }
+            })
     }
     signupUser(user) {
-      console.log(user);
-      Accounts.createUser({
-        username: user.username,
-        password: user.password,
-        profile:{
-          isAdmin: false,
-          company: user.company,
-          phone: user.phone,
-          email: user.email,
-          avg: 1,
-          createdAt: new Date
+        // console.log(user);
+        if (user.password != user.passwordTwo) {
+            alert('Passwords do not match!')
+        } else {
+            Accounts.createUser({
+                username: user.username,
+                password: user.password,
+                profile: {
+                    isAdmin: false,
+                    company: user.company,
+                    phone: user.phone,
+                    email: user.email,
+                    avg: 1,
+                    createdAt: new Date
+                }
+            }, function(err) {
+                if (err) {
+                    alert("you messed up")
+                    console.log(err);
+                } else {
+                    alert('succes on user creation!')
+                }
+            });
         }
-      }, function(err){
-        if(err){
-          alert("you messed up")
-          console.log(err);
-        }else{
-          alert('succes on user creation!')
-        }
-      });
     }
 
-    loginUser(username, password){
-      // console.log(username, password);
-      Meteor.loginWithPassword(username, password, function(err){
-        if(err){
-          console.log(err);
-        }else {
-          alert("signed in")
-          $state.go('/scheduleRequest')
-        }
-      });
+    loginUser(username, password) {
+        // console.log(username, password);
+        Meteor.loginWithPassword(username, password, function(err) {
+            if (err) {
+                console.log(err);
+            } else {
+                alert("signed in")
+                $state.go('/scheduleRequest')
+            }
+        });
     }
 
-    logOutUser(){
-      console.log('logging out');
-      Meteor.logout();
+    logOutUser() {
+        console.log('logging out');
+        Meteor.logout();
     }
 
     toggle() {
-      this.hide = this.hide === false ? true: false;
+        this.hide = this.hide === false ? true : false;
     }
 
 }
@@ -78,10 +86,10 @@ export default angular.module('login', [
     .config(config);
 
 function config($stateProvider, $urlRouterProvider) {
-  'ngInject';
-  $stateProvider
-    .state('login', {
-      url: '/login',
-      template: '<login></login>'
-    });
+    'ngInject';
+    $stateProvider
+        .state('login', {
+            url: '/login',
+            template: '<login></login>'
+        });
 }
